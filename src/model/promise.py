14c2950a15764df06530fe09ecf421b1904bba57
Klimov Ilya <917-kis19u486@users.noreply.git.iu7.bmstu.ru>
2023-03-14 17:13:09 +0300

    [#5] add first version

--- src/model/promise.py
+++ src/model/promise.py
@@ -3,6 +3,9 @@ import requests
 
 
 def download_nasa(name: str, dataset_path: str) -> str:
+    if name not in ('cm1', 'jm1', 'kc1', 'kc2', 'pc1'):
+        raise ValueError('Invalid repository name')
+
     filename = f'{name}.arff'
     url = f'http://promise.site.uottawa.ca/SERepository/datasets/{filename}'
 
